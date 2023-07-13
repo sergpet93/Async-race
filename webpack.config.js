ad9ce2725bf66f530954c4988b417bb204a86d47
sergpet93 <maknev96@mail.ru>
2023-07-13 15:13:12 +0300

    feat: add setting config

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 8080
+   },
+  };
+  module.exports = (env) => ({
+   mode: env.development ? 'development' : 'production',
+   entry: './src/index.ts',
+   output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: 'assets/[name][ext]',
+   },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+       },
+       {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+       },
+       {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+       },
+       {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+       },
+       {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+       },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ...esLintPlugin(env.development),
+ ],
+ ...devServer(env.development)
+});
